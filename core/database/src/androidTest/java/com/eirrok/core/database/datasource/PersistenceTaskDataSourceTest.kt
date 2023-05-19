28909b9ff6af4e7fb3093d7af13b66c03763c345
albert gonzalez <eirrok@proton.me>
2023-05-19 12:44:10 +0200

    add basics test for database module

--- core/database/src/androidTest/java/com/eirrok/core/database/datasource/PersistenceTaskDataSourceTest.kt
+++ core/database/src/androidTest/java/com/eirrok/core/database/datasource/PersistenceTaskDataSourceTest.kt
@@ -4,7 +4,13 @@ import android.content.Context
 import androidx.room.Room
 import androidx.test.core.app.ApplicationProvider
 import com.eirrok.core.database.AppDatabase
+import com.eirrok.core.database.model.TaskEntity
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.test.runTest
+import org.junit.Assert
 import org.junit.Before
+import org.junit.Test
+import java.time.OffsetDateTime
 
 class PersistenceTaskDataSourceTest {
 
@@ -20,4 +26,30 @@ class PersistenceTaskDataSourceTest {
         ).build()
         persistenceTaskDataSource = appDatabase.taskDataSource()
     }
+
+    @Test
+    fun persistenceTaskDataSource_get_task_since_yesterday_is_empty() = runTest {
+        val tasks = persistenceTaskDataSource.getAllSinceYesterday().first()
+        Assert.assertEquals(0, tasks.size)
+    }
+
+    @Test
+    fun persistenceTaskDataSource_insert_and_delete_task_should_get_empty_tasks() = runTest {
+        val task = testTaskEntity()
+        persistenceTaskDataSource.insertAll(task)
+        val taskInserted = persistenceTaskDataSource.getAllSinceYesterday().first().first()
+        persistenceTaskDataSource.delete(taskInserted)
+        val tasks = persistenceTaskDataSource.getAllSinceYesterday().first()
+        Assert.assertEquals(0, tasks.size)
+    }
+
+    private fun testTaskEntity() =
+        TaskEntity(
+            uid = 0,
+            title = "title",
+            repeat = false,
+            remember = false,
+            completed = false,
+            date = OffsetDateTime.now()
+        )
 }
